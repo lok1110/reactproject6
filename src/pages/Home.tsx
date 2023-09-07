f4e1ae46218a56434aeaa80bb27dfd8eb42736fd
Lok <oui19867@gmail.com>
2023-09-07 21:17:08 +0800

    lms table update v3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 
 import React, { useState, useEffect } from "react"
 import { useNavigate, Link } from "react-router-dom";
-import { useGlobalContext } from '../components/context/MyGlobalContext'; 
 export interface IHomePageProps {}
 
 
@@ -10,10 +9,10 @@ export interface IHomePageProps {}
 
 const Home: React.FunctionComponent<IHomePageProps> = (props) => {
   const navigate = useNavigate();
-  const { copy } = useGlobalContext()
+
   return (
     <div>
-        <p>{copy}</p> 
+   
       <Link to="/Profile">Go to Profile</Link>
       <button onClick={() => navigate('/layout/55')}>Go to layout</button>
       <h1>WELCOME TO HOME PAGE</h1>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,21 +5,20 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../aws-exports';
-import { useGlobalContext } from "../components/context/MyGlobalContext"; 
 
 Amplify.configure(awsconfig);
 
 export function Login({ signOut, user }: WithAuthenticatorProps) {
-  const navigate = useNavigate();
-  const { copy, setCopy } = useGlobalContext()
+ 
+
 
   useEffect(() => {
-    setCopy(user?.username)
+   
   }, [user])
 
   return (
     <>
-      {copy}
+    
       <button onClick={signOut}>Sign out</button>
     </>
   );
