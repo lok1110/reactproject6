d48fa96046600c03dd042a21398bd232ca1e35e7
Lok <oui19867@gmail.com>
2023-09-09 15:47:51 +0800

    update lms

--- src/pages/lms/LMSReport.tsx
+++ src/pages/lms/LMSReport.tsx
@@ -4,8 +4,30 @@ import moment from 'moment';
 import { API } from 'aws-amplify';
 import { GraphQLQuery } from '@aws-amplify/api';
 import DatePicker from 'react-datepicker';
-
+import "react-datepicker/dist/react-datepicker.css";
+import './LMS.css'
 import { ListSKGOPointLogsQuery,  SKGOPointLog} from '../../API';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Container,
+  Center,
+  Stack,
+  Box,
+} from '@chakra-ui/react'
 
 
 export const LMSReport = () => {
@@ -64,27 +86,61 @@ export const LMSReport = () => {
   
     return (
       <div>
-        <DatePicker
+      
+         <Box w='40px' h='40px' > 
+      
+        <DatePicker 
           selected={selectedMonth}
           onChange={(date) => setSelectedMonth(date as Date)}
           dateFormat="MM/yyyy"
+
           showMonthYearPicker
         />
+        </Box>
+    
+       
         {/* Render the data here */}
         {toLMS.length > 0 && toLMSNext.length > 0 && (
-          <div>
-            <p>第一筆資料:</p>
-            <p>Timestamp: {toLMS[0].Timestamp}</p>
-            <p>Points: {toLMS[0].PointDetails?.map((item) => item?.Value).join(", ")}</p>
-            <p>第二筆資料:</p>
-            <p>Timestamp: {toLMSNext[0].Timestamp}</p>
-            <p>Points: {toLMSNext[0].PointDetails?.map((item) => item?.Value).join(", ")}</p>
-            <p>相減結果:</p>
-            // 使用map函數和reduce函數來計算兩個數據的Points之差
-            <p>Points: {toLMS[0].PointDetails?.map((item, index) =>  Number(toLMSNext[0].PointDetails?.[index]?.Value) - Number(item?.Value)).reduce((a,b) => a + ", " + b, "")}</p>
-        
-          </div>
+          
+          <Box w='40px' h='40px' bg='yellow.200'>   
+            <Table size='sm' variant='striped'>
+              <Thead>
+                <Tr>
+                  <Th>Start Data:</Th>
+                  <Th>End Date:</Th>
+                  <Th>Result :End Date - Start Date</Th>
+                </Tr>
+
+
+                <Tr>
+                  <Td>{toLMS[0].Timestamp}</Td>
+                  <Td>{toLMSNext[0].Timestamp}</Td>
+                  <Td></Td>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>
+                    Points Value:<br />
+                    {toLMS[0].PointDetails?.map((item) => item?.Value).join('\n')}
+                  </Td>
+                  <Td>
+                  Points Value:<br />
+                    {toLMSNext[0].PointDetails?.map((item) => item?.Value).join('\n')}
+
+                  </Td>
+                  <Td>
+                    Result:
+                    {toLMS[0].PointDetails?.map((item, index) => Number(toLMSNext[0].PointDetails?.[index]?.Value) - Number(item?.Value)).reduce((a, b) => a + "\n" + b, "")}
+                  </Td>
+
+                </Tr>
+              </Tbody>
+            </Table>
+       </Box>
+         
         )}
       </div>
     )
   }
+  
